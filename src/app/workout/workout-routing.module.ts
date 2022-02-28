234d866802753973b58f59d421ba2818751abf90
pankowskitomasz <pankowski.email@gmail.com>
2022-02-28 20:12:06 +0200

    routing

--- src/app/workout/workout-routing.module.ts
+++ src/app/workout/workout-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ComingsoonComponent } from './comingsoon/comingsoon.component';
+import { WorkoutComponent } from './workout/workout.component';
 
 const routes: Routes = [
   {
-    path:"comingsoon",
-    component: ComingsoonComponent
+    path: "workout",
+    component: WorkoutComponent
   }
 ];
 
@@ -13,4 +13,4 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class ComingsoonRoutingModule { }
+export class WorkoutRoutingModule { }
---
+++ src/app/workout/workout/workout.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-workout',
+  templateUrl: './workout.component.html',
+  styleUrls: [
+    './workout.component.scss',
+    "../../sass/styles.scss"
+  ]
+})
+export class WorkoutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
